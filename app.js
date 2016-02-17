6c7cadc8d8277d6052091799a0ebc0a2121166e9
Student <m.d.oroark@gmail.com>
2016-02-17 16:52:06 -0700

    first commit finished bonuses

---
+++ app.js
@@ -0,0 +1,62 @@
+// Requires \\
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var fs = require('fs')
+// Create Express App Object \\
+var app = express();
+var places = require('./info.js')
+
+
+
+// Application Configuration \\
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+// Routes \\
+var destinations = ['destination1' ,'destination2', 'destination3','destination4','destination5','destination6']
+
+app.get('/', function(req, res){
+  res.sendFile('public/main.html', {root : './'})
+})
+
+app.get('/:destination', function(req,res,next){
+	var goodLocation = null;
+	for(var i = 0; i < destinations.length; i++){
+		if (req.params.destination === destinations[i])	{
+			goodLocation = true
+  				
+		} 
+	}
+	if (goodLocation){
+		res.sendFile('public/' + req.params.destination + '.html', {root : './'})
+		console.log(req.query , '!!!')
+		if (req.query.good === 'true') {
+		 res.json(places[0])
+		}
+	}
+	else{
+		next()
+	}
+})
+
+app.use(function(req,res){
+	res.send('you cant go here because its not a part of magellans voyage')
+});
+
+
+// app.use(function(err, req, res, next) {
+//   console.log('404 error')
+//   res.status(err.status || 404);
+//   res.send('not handled by other routes')
+// });
+
+
+// Creating Server and Listening for Connections \\
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+})
+
---
+++ info.js
@@ -0,0 +1,28 @@
+var places = [
+	
+	{
+		location: 'seville',
+		nextLocation : 'canary islands',
+		},
+	{
+		location: 'canary islands',
+		nextLocation : 'cape verde',
+	},
+	{
+		location: 'cape verde',
+		nextLocation: 'strait of magellan',
+	},
+	{
+		location: 'strait of magellan',
+		nextLocation: 'Guam',
+	},
+	{
+		location: 'Guam',
+		nextLocation: 'Phillipines'
+	},
+	{
+		location: 'Phillipines'
+	}
+
+]
+module.exports=places
